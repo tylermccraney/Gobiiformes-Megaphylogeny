6052a14aaf965b6845dc1c5286041c6cbbe48039
Tyler McCraney <tmccraney@g.ucla.edu>
2015-02-21 03:43:26 +0000

    Shell script that commands PHLAWD to assemble rag1 alignment.

---
+++ rag1PHLAWD.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## Tyler McCraney
+## February 20, 2015
+
+## This shell script commands PHLAWD to assemble an alignment for a single gene (rag1)
+## rag1.configfile is the configuration file that specifies parameters of the PHLAWD run  
+
+# Run PHLAWD
+PHLAWD assemble rag1.configfile
