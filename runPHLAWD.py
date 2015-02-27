84d3a19beb0bb51412f4312d86989867f2779bfb
Tyler McCraney <tmccraney@g.ucla.edu>
2015-02-27 01:48:11 +0000

    Tested out function version of makeconfigfiles.py

--- runPHLAWD.py
+++ runPHLAWD.py
@@ -10,17 +10,13 @@ import os
 # Tyler McCraney
 # February 25, 2015
 
-# This program automates PHLAWD to analyze multiple genes
+def PHLAWDrunner(): # This function automates PHLAWD to analyze multiple genes
+    configfiles = glob.glob("*.configfile") # Create list of configfiles
+    print(str(len(configfiles)) + " genes will be analyzed by PHLAWD") # Print message and run PHLAWD    
+    for configfile in configfiles:
+        os.system("PHLAWD assemble " + configfile)
+        gene_name = configfile.replace(".configfile", "")
+        print(gene_name + " sequences aligned")
+    print("Finished " + str(len(configfiles)) + " PHLAWDs!") # Print message that everything is finished
 
-# Create list of configfiles
-configfiles = glob.glob("*.configfile")
-
-# Print message and run PHLAWD
-print(str(len(configfiles)) + " genes will be analyzed by PHLAWD")    
-for configfile in configfiles:
-    os.system("PHLAWD assemble " + configfile)
-    gene_name = configfile.replace(".configfile", "")
-    print(gene_name + " sequences aligned")
-    
-# Print message that everything is finished
-print("Finished " + str(len(configfiles)) + " PHLAWDs!")
+PHLAWDrunner() # Call function
